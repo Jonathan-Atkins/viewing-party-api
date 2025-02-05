d35291afeb15f0ce8742f2d56607c55592a1f9c1
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-02-05 11:45:39 -0800

    setup

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::MoviesController < ApplicationController
   def index
-    # if params[:top_movies]
-  conn = Faraday.new(url: "https://api.themoviedb.org/3/movie/top_rated") do |faraday|
-    faraday.headers["Authorization"] = 'Bearer 00b9635b34e0ab1a6757822f885f9ff3'
-  end 
-  
-  
-  
+    url = "https://api.themoviedb.org/3/movie/top_rated"
+    conn = Faraday.new(url: url) do |faraday|
+      faraday.headers["Authorization"] = "Bearer #{Rails.application.credentials.tmdb[:key]}"
+      faraday.headers["Accept"] = "application/json"
+    end
+
+    response = conn.get
+    json = JSON.parse(response.body, symbolize_names: true)
+require 'pry'; binding.pry
+    render json: json
+  end
 end
--- spec/requests/api/v1/top_movies_request_spec.rb
+++ spec/requests/api/v1/top_movies_request_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 describe "Top Rated Movies", type: :request do
   it "returns top 20 movies" do
-    # get https://api.themoviedb.org/3/movie/top_rated
     
     get "/api/v1/movies", params: { top_movies: true }
     
@@ -10,7 +9,6 @@ describe "Top Rated Movies", type: :request do
     expect(response.status).to eq 200
 
     movies = JSON.parse(response.body, symbolize_names: true)[:data]
-
     expect(movies).to be_an(Array)
 
     movies.each do |movie|
@@ -21,6 +19,6 @@ describe "Top Rated Movies", type: :request do
       expect(attrs[:title]).to be_an(String)
       expect(attrs[:vote_average]).to be_an(Float)
     end
-    expect(movies.count).to eq
+    expect(movies.count).to eq 20
   end
 end
