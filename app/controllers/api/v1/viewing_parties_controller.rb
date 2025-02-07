61f4cecdce06440bc6c9ea7c7a0cfd26770d8b9c
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-02-07 12:27:03 -0800

    updating create_viewing_party controller

--- app/controllers/api/v1/viewing_parties_controller.rb
+++ app/controllers/api/v1/viewing_parties_controller.rb
@@ -1,3 +1,7 @@
-class Api:V1::ViewingPartiesController < ApplicationController
-  require 'pry'; binding.pry
+class Api::V1::ViewingPartiesController < ApplicationController
+  def create   
+    require 'pry'; binding.pry
+    required_params = [:name, :start_time, :end_time, :movie_id, :movie_title, :invitees]
+    missing_params  = required_params.select { |param| params[param].blank? }
+  end
 end
--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,4 +1,16 @@
 class ViewingParty < ApplicationRecord
   has_many :viewing_party_invitees
   has_many :users, through: :viewing_party_invitees
+
+  validates :name, presence: true
+  validates :invitees, presence: true
+  validate :movie_id_or_movie_title
+
+  private
+
+  def movie_id_or_movie_title
+    if movie_id.blank? && movie_title.blank?
+      errors.add(:base, "Movie ID or Title must be present")
+    end
+  end
 end
